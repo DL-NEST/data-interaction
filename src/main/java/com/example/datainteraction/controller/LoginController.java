df3e3d66411091a1acc85541333c111a9a0e7581
DL-NEST <DL-NEST@users.noreply.github.com>
2021-06-13 21:45:12 +0800

    正常

--- src/main/java/com/example/datainteraction/controller/LoginController.java
+++ src/main/java/com/example/datainteraction/controller/LoginController.java
@@ -1,4 +1,21 @@
 package com.example.datainteraction.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping(path="/login")
 public class LoginController {
+//    @Autowired
+    @PostMapping("/log")
+    public String Login(@RequestParam String username,
+                        @RequestBody String password){
+        return "OK";
+    }
+    @GetMapping("test")
+    public String test2(){
+        return "FEEWGV";
+    }
 }
--- src/main/java/com/example/datainteraction/controller/TestController.java
+++ src/main/java/com/example/datainteraction/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.example.datainteraction.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
@@ -9,4 +12,10 @@ public class TestController {
     public String test(){
         return "test";
     }
+    @PostMapping("/log")
+    public String Login(@RequestBody String username,
+                        @RequestBody String password){
+        return "OK";
+    }
 }
+
