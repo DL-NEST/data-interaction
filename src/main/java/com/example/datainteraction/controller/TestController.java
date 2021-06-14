697948ce3ac6be46fd96d5f438908ff42267d34c
DL-NEST <DL-NEST@users.noreply.github.com>
2021-06-14 23:26:25 +0800

    正常

--- src/main/java/com/example/datainteraction/controller/TestController.java
+++ src/main/java/com/example/datainteraction/controller/TestController.java
@@ -1,8 +1,7 @@
 package com.example.datainteraction.controller;
 
-import cn.hutool.json.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,9 +16,9 @@ public class TestController {
     @RequestMapping(value = "/test")
     private Object getRestResponse() {
         String url = "http://118.31.64.160:8081/api/v4/nodes/emqx@127.0.0.1/stats";
-        Map<String,Object>result = restTemplate.getForObject(url, Map.class);
-        System.out.print(result.get("data"));
-        return result.get("data");
+        Map result = restTemplate.getForObject(url, Map.class);
+        Object res = result.get("data");
+        return res;
     }
 }
 
--- src/test/java/com/example/datainteraction/DataInteractionApplicationTests.java
+++ src/test/java/com/example/datainteraction/DataInteractionApplicationTests.java
@@ -1,21 +1,23 @@
 package com.example.datainteraction;
 
-import com.example.datainteraction.entiy.User;
-import com.example.datainteraction.repository.Userrepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
-import javax.persistence.Id;
+import java.util.Map;
 
 @SpringBootTest
 class DataInteractionApplicationTests {
 	@Autowired		//自动导入配置
-	private Userrepository userrepository;		//映射配置
+	RestTemplate restTemplate;
 	@Test
-	void contextLoads() {
-		System.out.println(userrepository.findAll());
-//		User uu=new User();
-//		System.out.println(uu.getId());
+	private Object getRestResponse() {
+		String url = "http://118.31.64.160:8081/api/v4/nodes/emqx@127.0.0.1/stats";
+		Map result = restTemplate.getForObject(url, Map.class);
+		Object res = result.get("data");
+//        jsonObject.getString("subscribers.count");
+//        System.out.println(jsonObject.getString("subscribers.count"));
+		return res;
 	}
 }
