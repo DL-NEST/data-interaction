edf6ae7bc8cbdcf58aff146bcfe615446135858a
DL-NEST <DL-NEST@users.noreply.github.com>
2021-06-17 23:24:10 +0800

    静态测试

--- src/main/java/com/example/datainteraction/controller/TestController.java
+++ src/main/java/com/example/datainteraction/controller/TestController.java
@@ -2,13 +2,14 @@ package com.example.datainteraction.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import com.example.datainteraction.WebSocket.OneToManyWebSocket;
 
 import java.util.Map;
 
-@RestController
+@Controller
 public class TestController {
 
     @Autowired
@@ -20,6 +21,10 @@ public class TestController {
         Map result = restTemplate.getForObject(url, Map.class);
         Object res = result.get("data");
         return res;
+    }
+    @RequestMapping("/static")
+    public String test2(){
+        return "/test";
     }
 
 
