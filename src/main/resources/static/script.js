a11b71a5162510bd450368d6d7310f5f2f1c0083
DL-NEST <DL-NEST@users.noreply.github.com>
2021-06-18 13:42:37 +0800

    静态测试

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,86 +1,33 @@
-var chart    = document.getElementById('chart').getContext('2d'),
-    gradient = chart.createLinearGradient(0, 0, 0, 450);
+$(document).ready(function () {
+	$("a#pageLink").click(function () {
+		$("a#pageLink").removeClass("active");
+		$(this).addClass("active");
+	});
 
-gradient.addColorStop(0, 'rgba(0, 199, 214, 0.32)');
-gradient.addColorStop(0.3, 'rgba(0, 199, 214, 0.1)');
-gradient.addColorStop(1, 'rgba(0, 199, 214, 0)');
+	$(".btn-show-left-area").click(function () {
+		$(".left-area").removeClass("show");
+		$(".left-area").addClass("show");
+	});
 
+	$(".btn-show-right-area").click(function () {
+		$(".right-area").removeClass("show");
+		$(".right-area").addClass("show");
+	});
 
-var data  = {
-    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October', 'November','December' ],
-    datasets: [{
-			label: 'Applications',
-			backgroundColor: gradient,
-			pointBackgroundColor: '#00c7d6',
-			borderWidth: 1,
-			borderColor: '#0e1a2f',
-			data: [60, 45, 80, 30, 35, 55,25,80,40,50,80,50]
-    }]
-};
+	$(".btn-close-right").click(function () {
+		$(".right-area").removeClass("show");
+	});
 
-var options = {
-	responsive: true,
-	maintainAspectRatio: true,
-	animation: {
-		easing: 'easeInOutQuad',
-		duration: 520
-	},
-	scales: {
-		yAxes: [{
-      ticks: {
-        fontColor: '#5e6a81'
-      },
-			gridLines: {
-				color: 'rgba(200, 200, 200, 0.08)',
-				lineWidth: 1
-			}
-		}],
-    xAxes:[{
-      ticks: {
-        fontColor: '#5e6a81'
-      }
-    }]
-	},
-	elements: {
-		line: {
-			tension: 0.4
-		}
-	},
-	legend: {
-		display: false
-	},
-	point: {
-		backgroundColor: '#00c7d6'
-	},
-	tooltips: {
-		titleFontFamily: 'Poppins',
-		backgroundColor: 'rgba(0,0,0,0.4)',
-		titleFontColor: 'white',
-		caretSize: 5,
-		cornerRadius: 2,
-		xPadding: 10,
-		yPadding: 10
+	$(".btn-close-left").click(function () {
+		$(".left-area").removeClass("show");
+	});
+});
+
+$('.main-area').scroll( function() {
+	if ($('.main-area').scrollTop() >= 88) {
+		$('div.main-area-header').addClass('fixed');
 	}
-};
-
-var chartInstance = new Chart(chart, {
-    type: 'line',
-    data: data,
-		options: options
-});
-
-document.querySelector('.open-right-area').addEventListener('click', function () {
-    document.querySelector('.app-right').classList.add('show');
-});
-
-document.querySelector('.close-right').addEventListener('click', function () {
-    document.querySelector('.app-right').classList.remove('show');
-});
-
-document.querySelector('.menu-button').addEventListener('click', function () {
-    document.querySelector('.app-left').classList.add('show');
-});
-
-document.querySelector('.close-menu').addEventListener('click', function () {
-    document.querySelector('.app-left').classList.remove('show');
+	else {
+		$('div.main-area-header').removeClass('fixed');
+	}
 });
